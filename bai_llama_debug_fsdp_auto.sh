2b3e8b33d53610054d319773d29470c0dfbdc503
Junbum Lee <junbum@lablup.com>
2026-01-27 15:52:04 +0900

    Create bai_llama_debug_fsdp_auto.sh for training setup
    This script sets up environment variables and runs a distributed training job using torchrun.

---
+++ bai_llama_debug_fsdp_auto.sh
@@ -0,0 +1,28 @@
+export LOG_RANK=0
+export NODE_RANK=$BACKENDAI_CLUSTER_LOCAL_RANK   # or 1 on the second node
+export GLOO_SOCKET_IFNAME=eth0
+export CONFIG_FILE=llama3_debug_fsdp.toml
+export GPU_COUNT=$(nvidia-smi -L | wc -l)
+
+## Debugs
+# export NCCL_DEBUG=INFO
+# export NCCL_DEBUG=WARN
+# export TORCH_DISTRIBUTED_DEBUG=DETAIL
+# export NCCL_DEBUG=TRACE NCCL_DEBUG_SUBSYS=SHM
+
+echo "Config file: $CONFIG_FILE"
+# cat $CONFIG_FILE
+
+echo "NODE_RANK: $NODE_RANK"
+echo "GPU_COUNT: $GPU_COUNT"
+export NCCL_UNIQUE_ID=asdf
+
+nvidia-smi --query-compute-apps=pid --format=csv,noheader | xargs -r kill -9
+kill -9 $(lsof -t -i:29500)
+sleep 5
+
+export NCCL_CUMEM_HOST_ENABLE=0 # /dev/shm UTF-8 NCCL Error fix
+
+torchrun --nnodes=$BACKENDAI_CLUSTER_SIZE --nproc_per_node=$GPU_COUNT --node_rank=$NODE_RANK \
+  --rdzv_backend=static --rdzv_endpoint="main1:29500" \
+  -m torchtitan.train --job.config_file $CONFIG_FILE
